a93e017ebcbf1a88d81fc8100706da48d9c9ff73
evaparicio Evelyn <ev.aparicio.c@gmail.com>
2017-11-10 00:27:23 +0000

    automatically backed up by learn

--- flow-control.js
+++ flow-control.js
@@ -23,11 +23,8 @@ function ageChecker(age) {
 }
 
 function ternaryTeenager(age) {
-if (age>=13 && age<=19) {
-  return "You are a teenager"
-} else{
-  return "You are not a teenager"
-}
+  //conditionToTest ? valueToBeReturnedIfTrue : valueToBeReturnedIfFalse
+age>=13 && age<=19 ? "You are a teenager" : "You are not a teenager"
 }
 
 function switchAge(age) {
